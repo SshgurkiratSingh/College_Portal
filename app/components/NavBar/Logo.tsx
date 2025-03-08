3f946ac90b3b6e1634470c0fe481b954f602f07e
Jass267 <xxgodlike30xx@gmail.com>
2025-03-08 23:00:39 +0530

    4

--- app/components/NavBar/Logo.tsx
+++ app/components/NavBar/Logo.tsx
@@ -1,20 +1,32 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { use } from "react";
 import ClientOnly from "../ClientOnly";
+
 const Lgog = () => {
   const router = useRouter();
+
   const handleClick = () => {
     router.push("/");
   };
+
   return (
     <>
       <ClientOnly>
-        <p onClick={handleClick} className="titBlock text-4xl">
-          <Image src="/images/ccetLogo.png" alt="Logo" width={40} height={40} />
-         
-        </p>
+        <div
+          onClick={handleClick}
+          className="flex items-center cursor-pointer"
+        >
+          <Image
+            src="/images/ccetLogo.png"
+            alt="Logo"
+            
+            width={100} // Adjusted width
+            height={100} // Adjusted height
+            className="object-contain" // Ensures the image scales properly
+          />
+          {/* Optional: Add text or other elements next to the logo */}
+        </div>
       </ClientOnly>
     </>
   );
