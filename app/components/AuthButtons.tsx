9f9002ddc0ded180e90eb43a6a55031acf8db3f4
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2025-03-08 08:45:34 +0530

    backup

--- app/components/AuthButtons.tsx
+++ app/components/AuthButtons.tsx
@@ -6,22 +6,7 @@ import React from "react";
 const AuthButtons = () => {
   return (
     <div className="flex justify-center space-x-4">
-      <button
-        onClick={() =>
-          document.dispatchEvent(new CustomEvent("open-login-modal"))
-        }
-        className="btn btn-primary px-6 py-2"
-      >
-        Sign In
-      </button>
-      <button
-        onClick={() =>
-          document.dispatchEvent(new CustomEvent("open-register-modal"))
-        }
-        className="btn btn-outline px-6 py-2"
-      >
-        Register
-      </button>
+
     </div>
   );
 };
